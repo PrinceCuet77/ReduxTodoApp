edb4131543a53e0c20b7107d456ca1337a198096
Rezoan Shakil Prince <prince.cuet.77@gmail.com>
2024-04-23 09:35:42 +0600

    Redux Todo App (With Thunk): Remove README.md content & firebase url fix

--- src/store/todo/todoAPI.ts
+++ src/store/todo/todoAPI.ts
@@ -5,7 +5,7 @@ import { TodoItem } from './todo-slice'
 
 export const todoAPI = {
   getTodo: () => {
-    return axios.get(config.BASE_URL + config.DB + '.json')
+    return axios.get(`${config.BASE_URL}${config.DB}.json`)
   },
   postTodo: (todo: TodoItem) => {
     const data = { name: todo.name, isEditted: todo.isEditted }
@@ -14,13 +14,13 @@ export const todoAPI = {
     return axios.post(url, data)
   },
   updateTodo: (todo: TodoItem) => {
-    const url = `${config.BASE_URL}/${config.DB}/${todo.id}.json`
+    const url = `${config.BASE_URL}${config.DB}/${todo.id}.json`
     const data = { name: todo.name, isEditted: todo.isEditted }
 
     return axios.put(url, data)
   },
   deleteTodo: (id: string) => {
-    const url = `${config.BASE_URL}/${config.DB}/${id}.json`
+    const url = `${config.BASE_URL}${config.DB}/${id}.json`
     return axios.delete(url)
   },
 }
